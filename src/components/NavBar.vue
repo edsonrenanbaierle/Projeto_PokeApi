a5c031b14587cd5a3179bc06fed87b53f8c99b2b
Edson Baierle <edsonbaierle@gmail.com>
2024-01-16 16:37:07 -0300

    NavBar base da aplicação

---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav>
+     <router-link to="/" id="logo_position">
+       <img src="../assets/pokemon_logo.png" alt="Imagem_logo" id="logo_url">
+     </router-link>
+     <router-link to="/">Pokemons</router-link>
+     <router-link to="/SobrePokemon">Sobre</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+  nav{
+    padding: 15px 20px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: #E53935;
+  }
+
+  #logo_url{
+    height: 70px;
+    width: 100px;
+  }
+
+  #logo_position {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  nav a {
+    color: #ffc935;
+    text-decoration: none;
+    margin: 12px;
+    transition: 0.5s;
+
+  }
+
+  nav a:hover {
+    color: #FFF;
+  }
+
+
+</style>
---
+++ src/views/SobrePokemon.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Historia</h1>
+</template>
+
+<script>
+export default {
+  name:'SobrePokemon'
+}
+</script>
